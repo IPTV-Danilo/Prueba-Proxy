f01cf030bca29ec82d5af958b08a6a3d92523b0f
Daniel <danilocrypto.81@gmail.com>
2026-04-14 11:19:31 -0300

    Update capture.py

--- capture.py
+++ capture.py
@@ -16,19 +16,24 @@ async def intentar_captura(proxy):
             )
 
             context = await browser.new_context(
-                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+                locale="es-AR"
             )
 
             page = await context.new_page()
 
             m3u8_links = []
 
+            # capturar requests
             page.on("request", lambda request: (
                 m3u8_links.append(request.url)
                 if ".m3u8" in request.url else None
             ))
 
+            print("🌍 Abriendo página...")
             await page.goto(URL, timeout=60000)
+
+            # esperar carga del player
             await page.wait_for_timeout(15000)
 
             await browser.close()
